d0b9f0bb864ee64f2b67ca6719324545452d0204
waytogo <pearcesdecker@gmail.com>
2013-02-20 21:17:37 -0500

    Finalized HW2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,48 @@
 # in app/controllers/movies_controller.rb
-
+class InvalidFilterSortError < StandardError ; end
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+	@all_ratings = Movie.all_ratings
+	#ensure that there is either new sorting input or the old sorting settings are used
+	if params[:sort] != nil
+		@sort = params[:sort]
+	else
+		@sort = session[:sort]
+		@redirect = true
+	end
+	session[:sort] = @sort
+	#ensure that there is new ratings input or old ratings in session[] else no filters
+	if params[:ratings] == nil && session[:ratings] == nil #no new or old filters
+		@selected_ratings = Movie.all_ratings
+		session[:ratings] = Hash[@selected_ratings.map {|i| [i, true]}]
+		@redirect = true
+	elsif params[:ratings] == nil && session[:ratings] != nil #for all boxes unchecked case
+		@selected_ratings = session[:ratings].keys
+		@redirect = true
+	elsif params[:ratings] != nil #if new filters
+		@selected_ratings = params[:ratings].keys
+		session[:ratings] = params[:ratings]
+	else #if no new filters 
+		@selected_ratings = session[:ratings].keys
+		@redirect = true
+	end
+	#ensure that inputs are valid
+	if (@sort == nil || @sort == 'title' || @sort == 'release_date') && ((@selected_ratings & @all_ratings) == @selected_ratings)
+		#if a filter or sort gets re-used, we must redirect with both inputs filled so that all info is present in the URI
+		if @redirect == true
+			flash.keep
+			redirect_to movies_path(:sort => @sort, :ratings => session[:ratings])
+		end
+		@movies = Movie.find_all_by_rating(@selected_ratings, :order => @sort)
+		if (@sort == 'title')
+			@title_class = 'hilite'
+		elsif (@sort == 'release_date')
+			@release_date_class = 'hilite'
+		end
+	#else 
+	else
+		raise InvalidFilterSortError
+	end
   end
 
   def show
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :description, :release_date
+	attr_accessible :title, :rating, :description, :release_date
+	def self.all_ratings
+		@all_ratings = ['G','PG','PG-13','R','NC-17']
+	end
 end
